04fa5955686e9990773ff7c8bdc8403c9ea1eb3b
WojciechGw <wojciech.gwiozdzik@gmail.com>
2025-11-09 13:56:02 +0100

    progres

---
+++ src/qasm.c
@@ -0,0 +1,110 @@
+#include <rp6502.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+static const char * menu[4] =  {"\xFE", "File", "Edit", "Tools"};
+static const char * menu_dot[2] = {"setup", "about"};
+static const char * menu_file[4] =  {"new", "load", "save", "save as ..."};
+static const char * menu_edit[3] =  {"cut", "copy", "paste"};
+static const char * menu_tools[3] =  {"build", "run", "trace"};
+
+#define VEC_IRQ 0xFFFA
+#define VEC_RST 0xFFFC
+#define VEC_NMI 0xFFFE
+
+volatile uint8_t *vecIRQ = (volatile uint8_t *)VEC_IRQ;
+volatile uint8_t *vecRST = (volatile uint8_t *)VEC_RST;
+volatile uint8_t *vecNMI = (volatile uint8_t *)VEC_NMI;
+
+typedef struct {
+    uint16_t pc;  /* 0,1 */
+    uint8_t  p;   /* 2 */
+    uint8_t  a;   /* 3 */
+    uint8_t  x;   /* 4 */
+    uint8_t  y;   /* 5 */
+    uint8_t  s;   /* 6 */
+} CpuState;
+
+extern CpuState cpu_state;
+extern void capture_cpu_state(void);
+
+CpuState cpu_state;
+
+void print_bin8(uint8_t v){
+    int i = 0;
+    for (i = 7; i >= 0; i--) {
+        putchar( (v & (1u << i)) ? '1' : '0' );
+    }
+}
+
+void cls(){
+    // printf("\x1b[2J");
+    printf("\x1b");
+    printf("c");
+}
+
+void setpos(uint8_t row, uint8_t col){
+    printf("\x1b[%d;%dH", row, col);
+}
+
+void printat(uint8_t row, uint8_t col, char *txt){
+    printf("\x1b[%d;%dH%s", row, col, txt);
+}
+
+void show_CPU_status(){
+
+    uint16_t IRQVECaddr = vecIRQ[0] | (vecIRQ[1] << 8);
+    uint16_t RSTVECaddr = vecRST[0] | (vecRST[1] << 8);
+    uint16_t NMIVECaddr = vecNMI[0] | (vecNMI[1] << 8);
+
+    setpos(30,49);
+    printf("IRQ 0x%04X RST 0x%04X NMI 0x%04X", IRQVECaddr, RSTVECaddr, NMIVECaddr);
+    asm("jsr _capture_cpu_state");
+    printat(27, 53, "PC   SR AC XR YR SP NV-BDIZC");
+    setpos(28, 53);
+    printf("%04X %02X %02X %02X %02X %02X ", cpu_state.pc, cpu_state.p, cpu_state.a, cpu_state.x, cpu_state.y, cpu_state.s);
+    print_bin8(cpu_state.p);
+}
+
+void showMemory(uint16_t addrStart, uint16_t addrStop){
+    uint16_t addr = 0;
+    uint8_t i = 0;
+    for ( addr = addrStart; addr <= addrStop; addr += 16) {
+        uint8_t *p = (uint8_t *)addr;
+        printf("%04X ", addr);
+        for (i = 0; i < 16 && (addr + i) <= addrStop; i++) {
+            printf( (i == 8 ? "  %02X" : " %02X"), p[i]);
+        }
+        printf("\n");
+    }
+}
+
+void main(){
+
+    uint8_t i = 0;
+    uint8_t c = 1;
+
+    cls();
+    // printf("\33[48;2;%d;%d;%dm  ", 0x80, 0x80, 0x80);
+    printat(30, 1, menu[0]);
+    c = 3;
+    for(i = 1; i < (sizeof(menu) / sizeof(menu[0])); i++) {
+        printat(30, c-1, " ");
+        printat(30, c, menu[i]);
+        c += strlen(menu[i]) + 1;
+    }
+    // puts("\33[0m");
+
+    setpos(29,1);
+    for(i = 0; i < 80; i++) printf("_");
+
+    show_CPU_status();
+
+    setpos(1,1);
+    showMemory(0x8000,0x80FF);
+
+    printf("\n>");
+
+    while(1){}
+}
---
+++ src/qasm_cpustate.s
@@ -0,0 +1,86 @@
+.include "rp6502.inc"
+
+.export  _capture_cpu_state
+.import  _cpu_state
+
+.segment "ZEROPAGE"
+ret_lo:  .res 1   ; (PC-1).low
+ret_hi:  .res 1   ; (PC-1).high
+saveA:   .res 1
+saveX:   .res 1
+saveY:   .res 1
+saveP:   .res 1
+saveS:   .res 1
+
+.segment "CODE"
+
+; void capture_cpu_state(void);
+_capture_cpu_state:
+        ; --- zachowaj bieżące A,X,Y,S,P do tymczasów ---
+
+        sta saveA       ; A z miejsca wywołania
+        stx saveX       ; X z miejsca wywołania
+        sty saveY       ; Y z miejsca wywołania
+
+        tsx
+        stx saveS       ; S z miejsca wywołania
+
+        php             ; P -> stos
+        pla
+        sta saveP       ; P z miejsca wywołania
+
+        ; przywróć rejestry (żeby dalej program szedł w tym samym stanie)
+        lda saveA
+        ldx saveX
+        ldy saveY
+
+        ; --- pobierz PC (adres po JSR) i wpisz do cpu_state ---
+
+        jsr get_pc      ; wstawi PC do cpu_state.pc (offset 0,1)
+
+        ; --- wpisz P, A, X, Y, S do cpu_state ---
+
+        lda saveP
+        sta _cpu_state+2
+
+        lda saveA
+        sta _cpu_state+3
+
+        lda saveX
+        sta _cpu_state+4
+
+        lda saveY
+        sta _cpu_state+5
+
+        lda saveS
+        sta _cpu_state+6
+
+        rts
+
+; get_pc:
+; używa ret_lo/ret_hi, zapisuje PC bezpośrednio do _cpu_state+0/+1
+get_pc:
+        ; na stosie: adres powrotu (PC-1)
+        pla
+        sta ret_lo
+        pla
+        sta ret_hi
+
+        ; PC.low = (PC-1).low + 1
+        lda ret_lo
+        clc
+        adc #1
+        sta _cpu_state      ; offset 0 (low)
+
+        ; PC.high = (PC-1).high + carry
+        lda ret_hi
+        adc #0
+        sta _cpu_state+1    ; offset 1 (high)
+
+        ; przywróć oryginalne (PC-1) na stos (żeby RTS wrócił normalnie)
+        lda ret_hi
+        pha
+        lda ret_lo
+        pha
+
+        rts
