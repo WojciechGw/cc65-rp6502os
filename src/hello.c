5551c16a0e0b8c809e09dd7f9d5378742aa36228
WojciechGw <wojciech.gwiozdzik@gmail.com>
2025-11-09 13:55:13 +0100

    progres

--- src/hello.c
+++ src/hello.c
@@ -2,21 +2,29 @@
 #include <stdio.h>
 #include <stdint.h>
 
+char * menu[5] =  {"edit", "run", "save", "load", "quit"};
+
 void cls()
 {
     printf("\x1b[2J");
 }
 
-void setpos(uint8_t col, uint8_t row)
+void setpos(uint8_t row, uint8_t col)
 {
-    printf("\x1b[%d;%dH", col, row);
+    printf("\x1b[%d;%dH", row, col);
+}
+
+void printat(uint8_t row, uint8_t col, char *txt)
+{
+    printf("\x1b[%d;%dH%s", row, col, txt);
 }
 
 void main()
 {
     cls();
-    setpos(20,10);
-    puts("Hello, world!");
-    setpos(22,10);
-    printf("END\n");
+    printat(10,10,"Hello, world!");
+    printat(20,20,"END");
+    puts("\n");
+
+    while(1){}
 }
