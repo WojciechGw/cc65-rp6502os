b70bd328a23c02da13f3a0fbd541532d338372e4
WojciechGw <wojciech.gwiozdzik@gmail.com>
2025-12-26 10:40:32 +0100

    progres

--- assets/hello.asm
+++ assets/hello.asm
@@ -1,7 +1,10 @@
 ; Hello World for Mini ASSembler
 RIA_TX .equ $FFE1
+
     .org $C000
+    
     LDX #$00
+
 loop:
     LDA text,X
     STA RIA_TX
@@ -9,5 +12,9 @@ loop:
     CMP #$00
     BNE loop
     RTS
+
 text:
-.asciz "Hello, World!\r\n"
+.asciz "Hello, World I'm back!\r\n"
+
+runaddress:
+.word  $C000
